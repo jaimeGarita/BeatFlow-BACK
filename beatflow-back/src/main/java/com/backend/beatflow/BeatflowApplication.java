8cd34434220d9abe3a84769cfe97faace5d834ce
jaimeGarita <jaime.garita.camargo@gmail.com>
2023-05-27 19:32:56 +0200

    Añadido las configuraciones del front y el back

---
+++ beatflow-back/src/main/java/com/backend/beatflow/BeatflowApplication.java
@@ -0,0 +1,13 @@
+package com.backend.beatflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatflowApplication.class, args);
+	}
+
+}
---
+++ beatflow-back/src/test/java/com/backend/beatflow/BeatflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.beatflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
